74f60e298c04386ec5fa2d3d9110f1806462e8e1
Chase Pierce <syntaqx@gmail.com>
2023-04-14 17:37:17 -0600

    include security bits such as unattended-upgrades

--- .classic/bootstrap.sh
+++ .classic/bootstrap.sh
@@ -20,6 +20,9 @@ fi
 # Update apt package index
 sudo apt-get update
 
+# Install packages for automatic (unattended) security
+apt-get install -y apt-utils software-properties-common unattended-upgrades apt-listchanges jq
+
 # Install packages to allow apt to use a repository over HTTPS
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
