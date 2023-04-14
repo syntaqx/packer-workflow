c0ecfa89bf082bd7fd8e5f4671d87185934fd11f
Chase Pierce <syntaqx@gmail.com>
2023-04-14 17:42:25 -0600

    simplify to not talk about docker

--- .classic/bootstrap.sh
+++ .classic/bootstrap.sh
@@ -10,13 +10,6 @@ function wait_for_apt() {
 
 wait_for_apt
 
-# Ensure the $DOCKER_IMAGE variable is populated so we know which to start.
-DOCKER_IMAGE=${DOCKER_IMAGE:-}
-if [ -z "$DOCKER_IMAGE" ]; then
-    echo -e "\$DOCKER_IMAGE is empty or undefined, bootstrapping cannot occur"
-    exit 1
-fi
-
 # Update apt package index
 sudo apt-get update
 
@@ -26,39 +19,5 @@ apt-get install -y apt-utils software-properties-common unattended-upgrades apt-
 # Install packages to allow apt to use a repository over HTTPS
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Add Docker repository to apt sources
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update apt package index again
-sudo apt-get update
-
-# Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Add current user to docker group to avoid using sudo
-sudo usermod -aG docker $USER
-
-# Create systemd service file for Docker container
-cat <<EOF | sudo tee /etc/systemd/system/example.service > /dev/null
-[Unit]
-Description=My Docker Container
-
-[Service]
-Restart=always
-ExecStart=/usr/bin/docker run -p 80:80 $DOCKER_IMAGE
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Reload systemd
-sudo systemctl daemon-reload
-
-# Start Docker container service
-# sudo systemctl start example.service
-
-# Enable Docker container service to start at boot time
-sudo systemctl enable example.service
+# Install service requirements
+sudo apt-get install -y nginx
--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-# Packer & Docker Workflow Example
+# Packer Deployment Example
 
 This project demonstrates a very simple build and release system that leverages
-both [Docker](https://www.docker.com/) and [Packer](https://www.packer.io/) to
-build immutable artifacts that can be deployed easily on EC2.
-
-## Docker
-
-The actual _app_ runtime is containerized using Docker, which allows engineers
-a way to easily manage the expectations of the application.
+[Packer](https://www.packer.io/) to build immutable artifacts that can easily
+be deployed on EC2.
 
 ## Packer
 
-Packer is the operational runtime _for_ Docker. An AMI is built which is capable
-of starting the app as a Docker Service when the machine is deployed.
+Packer is used to execute the `.classic/bootstrap.sh` script, which allows full
+customization of the runtime for a given application. This can be generic and
+install all packages itself, start Docker, whatever is necessary.
+
+A more complex project likely would want to include a [File provisioner](https://developer.hashicorp.com/packer/docs/provisioners/file)
+to add source code for the runtime, or potentially even start a Docker container
+so that the operational runtime and app runtimes are separate.
 
 ## Deployment
 
 > WIP
 
-Deployment is very simple in that the AMI (The `ami_id` is output from the
-packer build job) needs to be started. This can be done on a singular EC2
-image, or via an Auto-Scaling group.
+Deployment is very simple in that you must only take the `ami_id` provided from
+the `build` stage and either deploy a single EC2 image or roll many out using
+an autoscaling group.
