20b146e7c10a6443e6c624c055d1de596ad63794
Chase Pierce <syntaqx@gmail.com>
2023-04-14 18:17:23 -0600

    simplify and prepare for terraform

--- .classic/bootstrap.sh
+++ .classic/bootstrap.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -aueo pipefail
+set -euo pipefail
 
-# Update apt package index
-sudo apt-get update
+main() {
+    # Update apt package index
+    sudo apt-get update
 
-# Install packages for automatic (unattended) security
-apt-get install -y apt-utils software-properties-common unattended-upgrades apt-listchanges jq
+    # Install packages for automatic (unattended) security
+    sudo apt-get install -y apt-utils software-properties-common unattended-upgrades apt-listchanges jq
 
-# Install packages to allow apt to use a repository over HTTPS
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+    # Install packages to allow apt to use a repository over HTTPS
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-# Install service requirements
-sudo apt-get install -y nginx
+    # Install service requirements
+    sudo apt-get install -y nginx
+}
+
+main "$@"
