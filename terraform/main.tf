c0a2d923f8f8cdeb14467c44af25b57ab66b7eac
Chase Pierce <syntaqx@gmail.com>
2023-04-14 18:29:38 -0600

    use variables in terraform and showcase the plan correctly

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,7 @@
-variable "ami_id" {
-  type = string
-}
-
 resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   ami           = var.ami_id
 }
+
+# TODO: This should be done via an ASG so we replace existing instances rather
+# than always deploying a new one.
